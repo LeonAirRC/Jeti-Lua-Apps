da746b32416bb44ca33cb284c1619828bcf87413
LeonAirRC <57962936+LeonAirRC@users.noreply.github.com>
2021-04-20 02:05:56 +0200

    Add files via upload

--- Timer Signalization Editor/BeepEdit.lua
+++ Timer Signalization Editor/BeepEdit.lua
@@ -1,7 +1,24 @@
---
--- Created by LeonAir RC
--- Date: 09.03.2021
---
+--[[
+Copyright (c) 2021 LeonAirRC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]
 
 local locale = system.getLocale()
 local appName = {en = "Beep Editor", de = "Beep Editor"}
@@ -187,6 +204,7 @@ end
 -- formID: 0, 1, 2 as specified by the Lua API, focused row for formID > 2
 -- if formID is <= 2 then the file selection is displayed, otherwise the list of elements is displayed as a table and the row 'formID - 2' is focused
 local function initForm(formID)
+    form.setTitle("")
     if (file and formID > 2) then -- a file is selected
         if (not elements) then -- load elements from file if not yet loaded
             local content = io.readall("/Config/" .. files[file] .. ".jsn") -- read content of file
