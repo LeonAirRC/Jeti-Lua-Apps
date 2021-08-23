c057e53b760eacb87e3b25bf94eca8e607b33d1d
LeonAirRC <leon.bartmann@udo.edu>
2021-08-23 13:13:28 +0200

    trimsteps 0.0.3

--- custom/Trim steps/trimsteps.lua
+++ custom/Trim steps/trimsteps.lua
@@ -21,17 +21,22 @@ SOFTWARE.
 ]]
 
 local voiceSwitch
-local trimSwitch
-local lastTrimVal
+local upSwitch, downSwitch
+local lastUpVal, lastDownVal
 local lastVoiceVal
 local step
 local trim
-local min, max = 0, 0
 
-local function trimSwitchChanged(value)
-    trimSwitch = value
-    system.pSave("trimsw", trimSwitch)
-    lastTrimVal = system.getInputsVal(trimSwitch)
+local function upSwitchChanged(value)
+    upSwitch = value
+    system.pSave("upsw", upSwitch)
+    lastUpVal = system.getInputsVal(upSwitch)
+end
+
+local function downSwitchChanged(value)
+    downSwitch = value
+    system.pSave("downsw", downSwitch)
+    lastDownVal = system.getInputsVal(downSwitch)
 end
 
 local function voiceSwitchChanged(value)
@@ -47,16 +52,16 @@ end
 
 local function printTelemetry(width, height)
     local text = tostring(math.floor(trim))
-    lcd.drawText((width - lcd.getTextWidth(FONT_MAXI, text)) // 2, (height - lcd.getTextHeight(FONT_MAXI)) // 2 - 5, text, FONT_MAXI)
-    local minText, maxText = tostring(math.floor(min)), tostring(math.floor(max))
-    lcd.drawText(width - lcd.getTextWidth(FONT_MINI, maxText) - 10, height - lcd.getTextHeight(FONT_MINI) - 5, maxText, FONT_MINI)
-    lcd.drawText(width - 40 - lcd.getTextWidth(FONT_MINI, minText), height - lcd.getTextHeight(FONT_MINI) - 5, minText, FONT_MINI)
+    lcd.drawText((width - lcd.getTextWidth(FONT_MAXI, text)) // 2, (height - lcd.getTextHeight(FONT_MAXI)) // 2, text, FONT_MAXI)
 end
 
 local function initForm()
     form.addRow(2)
-    form.addLabel({ label = "Trim switch" })
-    form.addInputbox(trimSwitch, true, trimSwitchChanged)
+    form.addLabel({ label = "Trim up" })
+    form.addInputbox(upSwitch, false, upSwitchChanged)
+    form.addRow(2)
+    form.addLabel({ label = "Trim down" })
+    form.addInputbox(downSwitch, false, downSwitchChanged)
     form.addRow(2)
     form.addLabel({ label = "Announcement switch", width = 200 })
     form.addInputbox(voiceSwitch, false, voiceSwitchChanged)
@@ -67,25 +72,31 @@ end
 
 local function init()
     voiceSwitch = system.pLoad("voicesw")
-    trimSwitch = system.pLoad("trimsw")
+    upSwitch = system.pLoad("upsw")
+    downSwitch = system.pLoad("downsw")
     step = system.pLoad("step", 1)
-    trim = 0
+    trim = system.pLoad("trim", 0)
     system.registerForm(1, MENU_APPS, "Trim steps", initForm)
-    system.registerTelemetry(2, "Trim steps", 2, printTelemetry)
-    lastTrimVal = system.getInputsVal(trimSwitch)
+    system.registerTelemetry(2, "Trim steps", 4, printTelemetry)
+    lastUpVal = system.getInputsVal(upSwitch)
+    lastDownVal = system.getInputsVal(downSwitch)
     lastVoiceVal = system.getInputsVal(voiceSwitch)
 end
 
 local function loop()
-    local trimVal = system.getInputsVal(trimSwitch)
-    if trimVal == -1 and lastTrimVal ~= -1 then
-        trim = trim - step
-    elseif trimVal == 1 and lastTrimVal ~= 1 then
+    local upVal = system.getInputsVal(upSwitch)
+    local downVal = system.getInputsVal(downSwitch)
+    if upVal == 1 and lastUpVal == -1 then
         trim = trim + step
+        system.playNumber(trim, 0, "")
+        system.pSave("trim", trim)
+    elseif downVal == 1 and lastDownVal == -1 then
+        trim = trim - step
+        system.playNumber(trim, 0, "")
+        system.pSave("trim", trim)
     end
-    if trim > max then max = trim end
-    if trim < min then min = trim end
-    lastTrimVal = trimVal
+    lastUpVal = upVal
+    lastDownVal = downVal
     local voiceVal = system.getInputsVal(voiceSwitch)
     if voiceVal == 1 and lastVoiceVal == -1 then
         system.playNumber(trim, 0, "")
@@ -93,4 +104,4 @@ local function loop()
     lastVoiceVal = voiceVal
 end
 
-return { init = init, loop = loop, author = "LeonAir RC", version = "0.0.1", name = "Trim steps" }
+return { init = init, loop = loop, author = "LeonAir RC", version = "0.0.3", name = "Trim steps" }
